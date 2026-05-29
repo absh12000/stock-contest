238bf01cc6d93803a0c6e95384865a980d916fcc
absh12000 <absh12000@gmail.com>
2026-05-29 17:54:38 +0900

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -13,8 +13,8 @@ SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=cs
 st.set_page_config(page_title="주식 동행", layout="wide")
 
 # --- [날짜 고정 설정] ---
-BASE_DATE = "20260511" 
-END_DATE = "20260529"    
+BASE_DATE = "20260604" 
+END_DATE = "20260630"    
 
 @st.cache_data(ttl=60) 
 def get_pure_closing_price(ticker, target_date):
